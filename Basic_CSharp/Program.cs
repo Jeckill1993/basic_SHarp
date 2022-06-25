5799d106f48bc354b8838d98b63edc2e30802cab
Jeckill1993 <evgeniyduplenko@gmail.com>
2022-06-25 12:40:20 +0300

    update basic theory (miss familiar parts)

--- Basic_CSharp/Program.cs
+++ Basic_CSharp/Program.cs
@@ -4,7 +4,15 @@ namespace Basic_CSharp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
+        {
+            Types();
+            Operations();
+            ConcatenationInterpolation();
+            Convertation();
+        }
+
+        static void Types()
         {
             //Main Types of Variables: int | uint | float | string
 
@@ -32,8 +40,43 @@ namespace Basic_CSharp
 
             //Boolean
             bool bl = true; // can contain true or false
+        }
 
-            Console.WriteLine("Hello World!");
+        static void Operations()
+        {
+
+            // Arithmetic operators
+            // + - * / % (as usual)
+            
+            int resultInt = 5 / 2; // 2, because we use two integer
+            float resultFloat = 5 / 2.0f; // 2.5, and assign it to type float (or 5.0 / 2) must be one of float number
+
+            // one more variant (convert type)
+            int x = 5;
+            int y = 2;
+            float result = Convert.ToSingle(x) / y;
+
+            // Logical operators (as usual)
+        }
+
+        static void ConcatenationInterpolation()
+        {
+            // Concatenation string + string = string, number + string = numberstring, etc...
+            // Interpolation
+            string name = "Jeckill";
+            string job = "doctor";
+            int age = 48;
+
+            string result = $"My name is {name}. I am a {job} and my age is {age} years"; // similar JS with ``
+        }
+
+        static void Convertation()
+        {
+            int age;
+            string num = "15";
+
+            age = Convert.ToInt32(num);
+            Console.WriteLine(age);
         }
     }
 }
